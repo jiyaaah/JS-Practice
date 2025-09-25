c1fd8f406b79338ec1bad92eef61b81369b77113
jiyaaah <jiyahona23@gmail.com>
2025-09-25 19:49:25 +0545

    feat: update first.js with new print message

--- first.js
+++ first.js
@@ -1,1 +1,6 @@
-uefuefjbehjf
+let getNum = 25;
+let userNum = prompt("Guess the game number");
+while(userNum!=getNum){
+    userNum = prompt("you guessedd the wrong number.guess again:");
+console.log("Congratulations!You entered the right number");
+}
